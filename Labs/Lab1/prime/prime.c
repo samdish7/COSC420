6a48e5e742ec3d6e693595f703bb62c5fa2fa701
samdish7 <samdish98@gmail.com>
2020-09-09 21:55:30 -0400

    Updated Lab 1

--- Labs/Lab1/prime/prime.c
+++ Labs/Lab1/prime/prime.c
@@ -3,20 +3,28 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<mpi.h>
-
+#include<math.h>
 int main(int argc, char** argv){
 	MPI_Init(NULL,NULL);
 	
 	MPI_Comm world = MPI_COMM_WORLD;
-	
+	if(argc !=2){
+		printf("Argument Error!\n");
+		MPI_Finalize();
+		return 1;
+	}
 	int num = atoi(argv[1]);
-	int worldSize, myRank;
-	int counter=0;
-	printf("Number passed is ~> %d",num);
+	int worldSize, myRank,i,split;
+
 	MPI_Comm_size(world, &worldSize);
 	MPI_Comm_rank(world, &myRank);
 	
-	
+	split = num / worldSize;
+
+	if (myRank == 0){
+		printf("The number passed is ~> %d\n",num);
+	}
+	i = myRank + 2;
 
 	MPI_Finalize();
 	return 0;
